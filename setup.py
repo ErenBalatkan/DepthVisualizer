e75baa17c51373df6d656e3cde95b3a36764dde5
Eren <e.balatkan@gmail.com>
2020-02-14 21:29:38 +0300

    PyPI Preperations

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Artifical Intelligence Research : Helper tool',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
