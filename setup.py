050e0bcc203c685a578074382d9fd6b284470d8d
Eren <e.balatkan@gmail.com>
2020-02-14 22:13:42 +0300

    Camera Starting Coordinate Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name="DepthVisualizer",
     packages=["DepthVisualizer"],
-    version='0.1',
+    version='0.11',
     license="MIT",
     description="Point Cloud & Depth Map visualization library",
     author="Eren Balatkan",
